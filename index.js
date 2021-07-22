a5ba8fb8c936b97fd00172a161ed42fd5c7596b3
Kutman <smanovkutman0258@gmail>
2021-07-22 13:08:57 +0600

    mobile2

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const app = express();
 
 const router = require('./routes/routes');
 
+//внедряем json
 app.use(express.json());
+
+//внедряем роутер
 app.use("/api", router);
 
+//создаём слушатель для порта 8080
 app.listen(PORT, () => console.log(`App listen port  + ${PORT}`));
